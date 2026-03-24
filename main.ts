c1511f0f2f178a7c191298941e132ad4e3b7e15d
Brandon-Tsang <brandon.tsang@stu.ocsb.ca>
2026-03-24 13:28:10 -0400

    Shows the comparison of 2 intergers

--- main.ts
+++ main.ts
@@ -1,8 +1,40 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Brandon
+ * Created on: Mar 2026
+ * This program will show a comparison between 2 random integers.
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber1: number = randint (0,99)
+let randomNumber2: number = randint (0,99)
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// display random number 1
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showString("#1: " + randomNumber1.toString())
+    basic.showIcon(IconNames.Happy)
+})
+
+// display random number 2
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showString("#2: " + randomNumber2.toString())
+    basic.showIcon(IconNames.Happy)
+})
+
+// display comparison
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    if (randomNumber1 > randomNumber2) {
+        basic.showString(randomNumber1.toString() + ">" + randomNumber2.toString()) 
+    }
+    else {
+        basic.showString(randomNumber1.toString() + "<" + randomNumber2.toString())
+    }
+    basic.showIcon(IconNames.Sad)
+})
